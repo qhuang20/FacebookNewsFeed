4496fd36bb7d8257f14fe3f8c9140aac6d4d3b8d
Jeffery Huang <jeffery@mymac.local>
2017-12-14 15:37:11 -0800

    Set up the root controller

--- FacebookNewsFeed/ViewController.swift
+++ FacebookNewsFeed/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Facebook Feed"
+        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
